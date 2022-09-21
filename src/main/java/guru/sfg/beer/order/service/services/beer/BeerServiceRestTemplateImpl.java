54c79ac58a0470ba76e34d74e4dc52c92962d554
Graham Bygrave <graham@bygrave.org>
2022-09-21 11:48:38 +0100

    Mess

---
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceRestTemplateImpl.java
@@ -0,0 +1,39 @@
+package guru.sfg.beer.order.service.services.beer;
+
+import guru.sfg.beer.order.service.web.model.BeerDto;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@Service
+public class BeerServiceRestTemplateImpl implements BeerService {
+
+    public static final String BEER_PATH_V1 = "/api/v1/beer/";
+    public static final String BEER_UPC_PATH_V1 = "/api/v1/beerUpc/";
+    private final RestTemplate restTemplate;
+
+    @Setter
+    private String beerServiceHost;
+
+    public BeerServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerById(UUID beerId) {
+        return Optional.of(restTemplate.getForObject(beerServiceHost + BEER_PATH_V1 + beerId, BeerDto.class));
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerByUpc(String upc) {
+        return Optional.of(restTemplate.getForObject(beerServiceHost + BEER_UPC_PATH_V1 + upc, BeerDto.class));
+    }
+}
---
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperDecorator.java
@@ -0,0 +1,39 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import guru.sfg.beer.order.service.services.beer.BeerService;
+import guru.sfg.beer.order.service.web.model.BeerDto;
+import guru.sfg.beer.order.service.web.model.BeerOrderLineDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+
+public class BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+
+    @Autowired
+    private BeerService beerService;
+
+    @Autowired
+    private BeerOrderLineMapper beerOrderLineMapper;
+
+    @Override
+    public BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line) {
+        BeerOrderLineDto orderLineDto = beerOrderLineMapper.beerOrderLineToDto(line);
+        Optional<BeerDto> beerDtoOptional = beerService.getBeerByUpc(line.getUpc());
+
+        beerDtoOptional.ifPresent(beerDto -> {
+            orderLineDto.setBeerId(beerDto.getId());
+            orderLineDto.setBeerName(beerDto.getBeerName());
+            orderLineDto.setBeerStyle(beerDto.getBeerStyle());
+            orderLineDto.setBeerPrice(beerDto.getPrice());
+        });
+
+        return orderLineDto;
+    }
+
+    @Override
+    public BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto) {
+        return beerOrderLineMapper.dtoToBeerOrderLine(dto);
+    }
+}

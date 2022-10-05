7ead2472f002b03367868dcf0579e6f834ec77f8
Graham Bygrave <graham@bygrave.org>
2022-10-05 17:03:48 +0100

    Send validate order request event when transition from new to validation pending. Closes #10.

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -2,6 +2,8 @@ package guru.sfg.beer.order.service.sm;
 
 import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
 import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.sm.actions.SendValidateOrderRequestAction;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
@@ -12,7 +14,11 @@ import java.util.EnumSet;
 
 @Configuration
 @EnableStateMachineFactory
+@RequiredArgsConstructor
 public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final SendValidateOrderRequestAction sendValidateOrderRequestAction;
+
     @Override
     public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
         states.withStates()
@@ -32,6 +38,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
                 .withExternal()
                 .event(BeerOrderEventEnum.VALIDATE_ORDER)
                 .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_PENDING)
+                .action(sendValidateOrderRequestAction)
                 .and()
                 .withExternal()
                 .event(BeerOrderEventEnum.VALIDATION_PASSED)
---
+++ src/main/java/guru/sfg/beer/order/service/sm/actions/SendValidateOrderRequestAction.java
@@ -0,0 +1,46 @@
+package guru.sfg.beer.order.service.sm.actions;
+
+import guru.sfg.beer.order.service.config.JmsConfig;
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import guru.sfg.beer.order.service.services.BeerOrderManagerImpl;
+import guru.sfg.beer.order.service.web.mappers.BeerOrderMapper;
+import guru.sfg.brewery.model.BeerOrderDto;
+import guru.sfg.brewery.model.events.ValidateOrderRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Send a validate order request message, typically when transitioning to the validate pending state.
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SendValidateOrderRequestAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final BeerOrderRepository beerOrderRepository;
+    private final JmsTemplate jmsTemplate;
+    private final BeerOrderMapper beerOrderMapper;
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> context) {
+        // Get/create the BeerOrderDTO
+        Optional.ofNullable(context.getMessage())
+                .map(msg -> (String) msg.getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER))
+                .ifPresent(orderId -> {
+                    BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(orderId));
+                    BeerOrderDto beerOrderDto = beerOrderMapper.beerOrderToDto(beerOrder);
+                    jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_QUEUE, new ValidateOrderRequest(beerOrderDto));
+                    log.debug("Sent Validation request to queue for order id " + orderId);
+                });
+    }
+}

2ee69be1fa0a58bf84a3c9935e7d44476b40b7c0
Graham Bygrave <graham@bygrave.org>
2022-10-25 14:17:28 +0100

    Update BOM and add Docker file for Docker Image. Closes #31.

--- src/test/java/guru/sfg/beer/order/service/BeerOrderServiceApplicationTests.java
+++ src/test/java/guru/sfg/beer/order/service/BeerOrderServiceApplicationTests.java
@@ -2,8 +2,10 @@ package guru.sfg.beer.order.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class BeerOrderServiceApplicationTests {
 
     @Test

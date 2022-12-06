e900b5d17c9d8f9e16d30679e08e6413be842d32
Graham Bygrave <graham@bygrave.org>
2022-12-06 11:23:36 +0000

    Switch off order tasting room service. Fix mapping error in customer. Switch to IP addresses when running in single-host docker compose mode.

--- src/main/java/guru/sfg/beer/order/service/web/mappers/CustomerMapper.java
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/CustomerMapper.java
@@ -20,11 +20,14 @@ package guru.sfg.beer.order.service.web.mappers;
 import guru.sfg.beer.order.service.domain.Customer;
 import guru.sfg.brewery.model.CustomerDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(uses = {DateMapper.class})
 public interface CustomerMapper {
 
+    @Mapping(target = "name", source = "customerName")
     CustomerDto customerToDto(Customer customer);
 
+    @Mapping(target = "customerName", source = "name")
     Customer dtoToCustomer(CustomerDto dto);
 }
